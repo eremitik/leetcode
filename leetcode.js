4ee83b467b0fa33839cb264d623185b55b60ca61
eremitik <39305876+eremitik@users.noreply.github.com>
2021-03-06 12:07:45 +0900

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -7,3 +7,14 @@ var defangIPaddr = function(address) {
     }
     return newArr.join('')
 }
+
+
+// 1480. Running Sum of 1d Array
+var defangIPaddr = function(address) {
+    let newAddy = address.split('')
+    let newArr = []
+    for(let i=0; i<newAddy.length; i++){
+        address[i] === '.' ? newArr.push('[.]') : newArr.push(address[i])
+    }
+    return newArr.join('')
+}
